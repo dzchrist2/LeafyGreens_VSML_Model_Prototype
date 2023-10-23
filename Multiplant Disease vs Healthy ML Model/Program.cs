382b1114af732c6fae0a7e6616f48627f19b621f
dzchrist2 <dzchrist@asu.edu>
2023-10-23 11:39:58 -0700

    Add project files.

---
+++ Multiplant Disease vs Healthy ML Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplant_Disease_vs_Healthy_ML_Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
